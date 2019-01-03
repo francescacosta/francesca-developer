1fe76af847b6fec061dc78deec85d13c745c1ff2
Francesca <francesca@thrivewb.com.au>
2019-01-03 11:49:42 +1000

    create work pages

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -8,13 +8,6 @@ import Layout from '../components/Layout'
 // Export Template for use in CMS preview
 export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
   <main className="Home">
-    <PageHeader
-      large
-      title={title}
-      subtitle={subtitle}
-      backgroundImage={featuredImage}
-    />
-
     <section className="section">
       <div className="container">
         <Content source={body} />
